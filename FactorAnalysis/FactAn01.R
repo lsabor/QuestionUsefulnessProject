fe5e7bdf33f123bd34064d5c5a64fd991f386420
lsabor <lukesabor@gmail.com>
2019-10-25 17:32:36 -0400

    second failed attempt

--- FactorAnalysis/FactAn01.R
+++ FactorAnalysis/FactAn01.R
@@ -1,5 +1,27 @@
-dataset = read.csv("InformativenessVsDifficultyR1 - Control.csv", row.names=1)
-cor.matrix = cor(dataset,dataset)
+datasets = list(read.csv("InformativenessVsDifficultyR1 - Control.csv", row.names=1),
+             read.csv("InformativenessVsDifficultyR1 - GJudge.csv", row.names=1),
+             read.csv("InformativenessVsDifficultyR2 - Control.csv", row.names=1),
+             read.csv("InformativenessVsDifficultyR2 - GJudge.csv", row.names=1))
 
-fa1 = factanal(covmat = cor.matrix, factors = 3, n.obs = nrow(dataset), rotation = "varimax")
-fa1
+cor.matrices = list(cor(datasets[[1]],datasets[[1]]),
+                 cor(datasets[[2]],datasets[[2]]),
+                 cor(datasets[[3]],datasets[[3]]),
+                 cor(datasets[[4]],datasets[[4]]))
+
+#THIS IS THE ONLY DATASET THAT WORKS, to try others, replace [[4]] with other numbers
+fan.result = factanal(covmat = cor.matrices[[4]],factors = 3, 
+                      n.obs = nrow(cor.matrices[[4]]), rotation = "varimax")
+fan.result
+
+# fan.results = list(factanal(covmat = cor.matrices[[1]],factors = 3, 
+#                             n.obs = nrow(cor.matrices[[1]]), rotation = "varimax"),
+#                    factanal(covmat = cor.matrices[[2]],factors = 3, 
+#                             n.obs = nrow(cor.matrices[[2]]), rotation = "varimax"),
+#                    factanal(covmat = cor.matrices[[3]],factors = 3, 
+#                             n.obs = nrow(cor.matrices[[3]]et), rotation = "varimax"),
+#                    factanal(covmat = cor.matrices[[4]],factors = 3, 
+#                             n.obs = nrow(cor.matrices[[4]]), rotation = "varimax")
+# )
+
+
+
